a5bde4b1dd4a66343d80d918225992146624011d
Michal Szczesny <michal6271@gmail.com>
2020-10-18 18:37:58 +0200

    Optymalizacja, dane posortowane w folderach

---
+++ Dane/readme.md
@@ -0,0 +1,9 @@
+Manually copy those files here:
+
+PC_Time2014.csv
+PC_Time2015.csv
+PC_Time2016.csv
+PC_Time2017.csv
+PC_Time2018.csv
+PC_Circuit.csv
+Circuit_Sector.csv
--- Skrypty/WalidacjaDanych.py
+++ Skrypty/WalidacjaDanych.py
@@ -1,6 +1,8 @@
 import fileinput
 import pandas as pd
 import time
+import os
+import shutil
 
 dataTabs = {'PC_Time2014' : set(), 'PC_Time2015' : set(), 
             'PC_Time2016': set(), 'PC_Time2017' : set(),
@@ -13,7 +15,7 @@ allTabNames = ['PC_Time2014', 'PC_Time2015', 'PC_Time2016',
                'PC_Time2017', 'PC_Time2018', 'Circuit_Sector',
                'PC_Circuit']
             
-sectorNames = ['S12', 'S23', 'S34', 'S45', 'S54', 'S65', 'S76', 'S81']
+sectorNames = ['S12', 'S23', 'S34', 'S45', 'S56', 'S67', 'S78', 'S81']
 
 tabSector = set()
 tabPC = set()
@@ -25,24 +27,35 @@ skippedRows = 0
 
 startTime = time.time()
 
-for sector in sectorNames:
-    for tab in allTabNames:
-        df = pd.read_csv('C:\Projekt_badawczy_CERN\cern\\' + tab + '.csv')
-        firstColumn = df.columns[0]
-        df = df.drop([firstColumn], axis=1)
-        df.to_csv('C:\Projekt_badawczy_CERN\cern\\' + tab + '__' + sector + '_no_duplicates.csv', index=False)
-        df = pd.read_csv('C:\Projekt_badawczy_CERN\cern\\' + tab + '__' + sector + '_no_duplicates.csv').drop_duplicates(keep='first').reset_index()
-        df.to_csv('C:\Projekt_badawczy_CERN\cern\\' + tab + '__' + sector + '_no_duplicates.csv', index=False)
+print("Filtration started...")
+
+# Clean directories
+if os.path.exists("./Dane/Po_filtracji"):
+    shutil.rmtree("./Dane/Po_filtracji")
+os.makedirs("./Dane/Po_filtracji")
 
 for sector in sectorNames:
-    for line in fileinput.FileInput('C:\Projekt_badawczy_CERN\cern\Circuit_Sector__' + sector + '_no_duplicates.csv', inplace=1):
+    os.makedirs("./Dane/Po_filtracji/" + sector)
+
+# Remove duplicates and save the result in another file
+for sector in sectorNames:
+    for tab in allTabNames:
+        df = pd.read_csv('./Dane/' + tab + '.csv')
+        firstColumn = df.columns[0]
+        df = df.drop([firstColumn], axis=1)
+        df.drop_duplicates(keep='first').reset_index()
+        df.to_csv('./Dane/Po_filtracji/' + sector + '/' + tab + '__' + sector + '.csv', index=False)
+
+# Get result files and filter out individual sectors
+for sector in sectorNames:
+    for line in fileinput.FileInput('./Dane/Po_filtracji/' + sector + '/Circuit_Sector__' + sector + '.csv', inplace=1):
         sectorData = line.split(',')
         if sectorData[3] == sector:
             validatedCircuits.add(sectorData[2])
             tabSector.add(line)
             print(line, end = '')
 
-    for line in fileinput.FileInput('C:\Projekt_badawczy_CERN\cern\PC_Circuit__' + sector + '_no_duplicates.csv', inplace=1):
+    for line in fileinput.FileInput('./Dane/Po_filtracji/' + sector + '/PC_Circuit__' + sector + '.csv', inplace=1):
         circuitData = line.split(',')
         if circuitData[1] not in validatedCircuits:
             skippedRows += 1
@@ -57,7 +70,7 @@ for sector in sectorNames:
     skippedRows = 0
 
     for tab in tabNames:
-        for line in fileinput.FileInput('C:\Projekt_badawczy_CERN\cern\\' + tab + '__' + sector + '_no_duplicates.csv', inplace=1):
+        for line in fileinput.FileInput('./Dane/Po_filtracji/' + sector +'/' + tab + '__' + sector + '.csv', inplace=1):
             tabData = line.split(',')
             if tabData[1] not in validatedPC:
                 skippedRows += 1
